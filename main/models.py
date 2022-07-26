57b52ead20ed8080552c47c857019ee7d6a41f79
Jinyoung2000 <sjy021621@gmail.com>
2022-07-27 00:16:15 +0900

    feat: create model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsData(models.Model):
+    title = models.CharField(max_length=300)
+    link = models.URLField()
+    news_id = models.CharField(max_length=15)
