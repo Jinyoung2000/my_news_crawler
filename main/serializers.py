21d4a1837685e9a08f31630aec0b1ecadd4bf291
Jinyoung2000 <sjy021621@gmail.com>
2022-07-27 22:19:04 +0900

    feat: 뉴스 리스트 가져오는 API 추가

---
+++ main/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import NewsData
+
+
+class NewsDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsData  # NewsData 모델 사용
+        fields = '__all__' # 필드 모두 포함
--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import NewsData
+from rest_framework.views import APIView
+from .serializers import NewsDataSerializer
+
+
+class NewsListAPI(APIView):
+    def get(self, request):
+        queryset = NewsData.objects.all()
+        print(queryset)
+        serializer = NewsDataSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 # Create your views here.
